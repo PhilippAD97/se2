57cbb024847a6bf8a0fadb1245ab9a5388ff4d73
Lion Ralfs <lion.ralfs@gmx.de>
2017-04-05 17:45:09 +0200

    Finished 1.4: 'Hello_World'

---
+++ Hello_World/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Prints `Hello World` to the console
+ * 
+ * @author Daniel Gooß, Lion Ralfs
+ * @version 0.0.1
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
